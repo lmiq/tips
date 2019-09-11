e10458d853a91abdc31c8155262c21412461b4ab
Leandro Martinez <leandro@iqm.unicamp.br>
2019-09-11 15:22:50 -0300

    improved path function

--- MyDist/startup.jl
+++ MyDist/startup.jl
@@ -4,13 +4,20 @@ println(" Loading .julia/config/startup.jl ")
 #using Revise ; println(" Loading Revise... ")
 
 function nogtk()
+  if "GKSwstype" in keys(ENV)
+    if ENV["GKSwstype"] == "nul"
+      return
+    end
+  end
   ENV["GKSwstype"]="nul" # This supresses the need of a display while plotting
   println("Will not display plot in GUI.")
 end
 
 function path(dir :: String)
- push!(LOAD_PATH,dir)
- println("Added $dir to LOAD_PATH")
+ if ! dir in LOAD_PATH
+   push!(LOAD_PATH,dir)
+   println("Added $dir to LOAD_PATH")
+ end
 end
 
 
